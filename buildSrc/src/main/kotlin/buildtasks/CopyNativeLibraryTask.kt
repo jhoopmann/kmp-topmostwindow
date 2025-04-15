6df966e41f52d62c0a04d6076d091c6cf20c1a5f
Jonas Hoopmann <contact@jhoopmann.de>
2025-04-15 21:38:54 +0200

    jdk 23

--- buildSrc/src/main/kotlin/buildtasks/CopyNativeLibraryTask.kt
+++ buildSrc/src/main/kotlin/buildtasks/CopyNativeLibraryTask.kt
@@ -1,12 +1,13 @@
 package buildtasks
 
-import org.gradle.process.*
-import org.gradle.api.tasks.*
-import org.gradle.api.provider.*
-import org.gradle.api.*
-import javax.inject.*
-import org.gradle.api.file.*
-import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.Directory
+import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.file.ProjectLayout
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ProviderFactory
+import org.gradle.api.tasks.TaskAction
+import javax.inject.Inject
 
 abstract class CopyNativeLibraryTask @Inject constructor(
     private val projectLayout: ProjectLayout,
@@ -15,10 +16,10 @@ abstract class CopyNativeLibraryTask @Inject constructor(
 ) : DefaultTask() {
     @TaskAction
     fun action() {
-        val targetJvmPlatform = providerFactory.gradleProperty("targetJvmPlatform").getOrNull() ?: "macos"
+        val targetJvmPlatform: String = providerFactory.gradleProperty("targetJvmPlatform").getOrNull() ?: "macos"
 
-        val projectNativeLibDir = projectLayout.projectDirectory.dir("src/${targetJvmPlatform}Main/native/build")
-        val buildNativeLibDir = projectLayout.buildDirectory.dir("native")
+        val projectNativeLibDir: Directory = projectLayout.projectDirectory.dir("src/${targetJvmPlatform}Main/native/build")
+        val buildNativeLibDir: Provider<Directory> = projectLayout.buildDirectory.dir("native")
 
         fileSystem.copy {
             from(projectNativeLibDir)
